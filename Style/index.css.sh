e96d4d622b2becf938cca172209a209eb8e3229b
Anton Leykin <antonleykin@gmail.com>
2025-08-03 11:20:31 -0400

    sidebar width = 1000px

--- Style/index.css.sh
+++ Style/index.css.sh
@@ -2,7 +2,7 @@
 # /*-*- css -*-*/
 ar () { echo "scale=2;" "$@" |bc; }
 pct () { echo `ar "scale=0; 100 * $1"`%; }
-barwid=280
+barwid=1000
 barun="px"
 limargin=`ar ".027 * $barwid"`
 lipadding=`ar ".027 * $barwid"`
